78c59d75ec8a4d3d77cc8c8ec212dcf8a0238aba
Unknown Eugene <kailas11252001@gmail.com>
2020-12-04 02:21:43 +0300

    Removed not used files

--- Lesson8/CardSystem/CardSecurity.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Lesson8.CardSystem
-{
-    public class CardSecurity
-    {
-        internal string Cvv2 { get; set; } = "111";
-        internal ushort PinCode { get; set; } = 0;
-    }
-}
